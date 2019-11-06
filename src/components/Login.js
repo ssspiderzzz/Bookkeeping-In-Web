f31a408c860acc314dddbfb712119f9e3ba3dcc1
Haibin Zhu <ssspiderzzz@gmail.com>
2019-11-06 00:19:41 -0800

    add new background image for login page only, fix ui bugs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Cookies from "js-cookie";
 import Button from "@material-ui/core/Button";
 import { GoogleLogin } from "react-google-login";
+import background from "./images/background/bg.jpg";
 import "./Login.css";
 
 export default function Login(props) {
@@ -23,29 +24,28 @@ export default function Login(props) {
 
   return (
     <React.Fragment>
-      <div id="bg">
-        {!props.auth.user && !props.auth.email && (
-          <div>
-            <br />
-            <GoogleLogin
-              clientId="680587798801-qp0mndlka16fgm91ed97gkoot3ru5145.apps.googleusercontent.com"
-              scope="profile"
-              buttonText="Sign in with Google"
-              uxMode="popup"
-              redirectUri="http://localhost:3000"
-              onSuccess={onSuccess}
-              onFailure={onFailure}
-              theme={"dark"}
-              cookiePolicy={"single_host_origin"}
-            />
-            <br />
-            <p>or</p>
-            <Button onClick={guestLogin} variant="contained" color="primary">
-              Guest Login
-            </Button>
-          </div>
-        )}
-      </div>
+      <img id="background" src={background} alt="background"></img>
+      {!props.auth.user && !props.auth.email && (
+        <div>
+          <br />
+          <GoogleLogin
+            clientId="680587798801-qp0mndlka16fgm91ed97gkoot3ru5145.apps.googleusercontent.com"
+            scope="profile"
+            buttonText="Sign in with Google"
+            uxMode="popup"
+            redirectUri="http://localhost:3000"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            theme={"dark"}
+            cookiePolicy={"single_host_origin"}
+          />
+          <br />
+          <p>or</p>
+          <Button onClick={guestLogin} variant="contained" color="primary">
+            Sign in as Guest
+          </Button>
+        </div>
+      )}
     </React.Fragment>
   );
 }

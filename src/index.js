fd77965ee3bafcd68a2a80422a0187593c3408ed
Haibin Zhu <ssspiderzzz@gmail.com>
2019-11-12 22:16:56 -0800

    set default axios settings for deployment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import axios from "axios";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+  axios.defaults.withCredentials = true;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));

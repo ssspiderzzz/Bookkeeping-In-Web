4964a35c1fb6116fbb5f778890b9ce3d25a6e2a0
Haibin Zhu <ssspiderzzz@gmail.com>
2020-01-14 13:14:13 -0800

    add dashboard columns

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,26 @@ import React from 'react'
 import './Dashboard.css'
 
 export default function Dashboard (props) {
+  const vertical_line = {
+    borderLeft: '1px solid grey',
+    height: '10px',
+    margin: '5px'
+  }
   return (
     <div id='dashboardContainer'>
       <div>Log in as {props.auth.user}</div>
+      <br />
+      <span>Income</span>
+      <span style={vertical_line}></span>
+      <span>Spending</span>
+      <span style={vertical_line}></span>
+      <span>Earning</span>
+      <br />
+      <span>0</span>
+      <span style={vertical_line}></span>
+      <span>2</span>
+      <span style={vertical_line}></span>
+      <span>13.31</span>
     </div>
   )
 }
